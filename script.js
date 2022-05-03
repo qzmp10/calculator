0f39c9eae4e889fe74bb0a162018246cbf8d1e0e
qzmp10 <loganrobitaille@outlook.com>
2022-05-03 10:08:25 -0400

    Added function that prevents people from bugs comin thru if user spams equal

--- script.js
+++ script.js
@@ -18,83 +18,93 @@ let times = document.querySelector('.times');
 let divide = document.querySelector('.divide');
 let equal = document.querySelector('.equal')
 
+//variables
 input.textContent = '';
 let firstNumber = '';
 let secondNumber = '';
 let operator = null;
 let numArray = [];
+let pressedEqual = false;
 
 
 one.addEventListener('click', () => {
-    // if(secondNumber !== '') {
-    //     input.textContent = '';
-    // } // this makes it so that if there is a secondNumber (usually after user presses equal) 
-    //the textContent will clear and give place to a new calculation (what happens w/ real calculator)
-
+    if(pressedEqual == true) { //if user previously pressed equal
+        input.textContent = ''; //reset textContent
+    }
     input.textContent += '1';
+    pressedEqual = false; // user presses button, so pressedEqual = false;
 });
 
 two.addEventListener('click', () => {
-    // if(secondNumber !== '') {
-    //     input.textContent = '';
-    // }
+    if(pressedEqual == true) { 
+        input.textContent = '';
+    }
     input.textContent += '2';
+    pressedEqual = false;
 });
 
 three.addEventListener('click', () => {
-    // if(secondNumber !== '') {
-    //     input.textContent = '';
-    // }
+    if(pressedEqual == true) { 
+        input.textContent = '';
+    }
     input.textContent += '3';
+    pressedEqual = false;
 });
 
 four.addEventListener('click', () => {
-    // if(secondNumber !== '') {
-    //     input.textContent = '';
-    // }
+    if(pressedEqual == true) { 
+        input.textContent = '';
+    }
     input.textContent += '4';
+    pressedEqual = false;
 });
 
 five.addEventListener('click', () => {
-    // if(secondNumber !== '') {
-    //     input.textContent = '';
-    // }
+    if(pressedEqual == true) { 
+        input.textContent = '';
+    }
     input.textContent += '5';
+    pressedEqual = false;
 });
 
 six.addEventListener('click', () => {
-    // if(secondNumber !== '') {
-    //     input.textContent = '';
-    // }
+    if(pressedEqual == true) { 
+        input.textContent = '';
+    }
     input.textContent += '6';
+    pressedEqual = false;
 });
 
 seven.addEventListener('click', () => {
-    // if(secondNumber !== '') {
-    //     input.textContent = '';
-    // }
+    if(pressedEqual == true) { 
+        input.textContent = '';
+    }
     input.textContent += '7';
+    pressedEqual = false;
 });
 
 eight.addEventListener('click', () => {
-    // if(secondNumber !== '') {
-    //     input.textContent = '';
-    // }
+    if(pressedEqual == true) { 
+        input.textContent = '';
+    }
     input.textContent += '8';
+    pressedEqual = false;
 });
 
 nine.addEventListener('click', () => {
-    // if(secondNumber !== '') {
-    //     input.textContent = '';
-    // }
+    if(pressedEqual == true) { 
+        input.textContent = '';
+    }
     input.textContent += '9';
+    pressedEqual = false;
 });
 
 zero.addEventListener('click', () => {
-    // if(secondNumber !== '') {
-    //     input.textContent = '';
-    // }
+    if(pressedEqual == true) { 
+        input.textContent = '';
+    }
     input.textContent += '0';
+    pressedEqual = false;
 });
 
 clear.addEventListener('click', () => {
@@ -103,19 +113,23 @@ clear.addEventListener('click', () => {
     secondNumber = '';
     operator = null;
     numArray = [];
+    pressedEqual = false;
 });
 
 equal.addEventListener('click', () => {
-    secondNumber = (input.textContent); // second number stored after user presses equal
-    let answer = operate(operator, firstNumber, secondNumber); // takes the operator pressed before with the two numbers and does the calculations
+    pressedEqual = true;
+    secondNumber = (input.textContent); 
+    preventEqualSpam();
+    let answer = operate(operator, firstNumber, secondNumber); 
     input.textContent = answer;
-    firstNumber = ''; //this makes it so that the firstNumber is deleted after the operation, so if user presses equal again, only secondNumber is processed
+    firstNumber = '';
     numArray = [];
 });
 
 plus.addEventListener('click', () => {
-    firstNumber = (input.textContent); 
-    arrayMagic();
+    firstNumber = (input.textContent); //sets firstNumber
+    arrayMagic(); //IF array.length !== 2 -> skips this step, goes to next operator -> ('plus')
+    // ELSE -> takes the previous operator and operates the two numbers in the array BEFORE moving on to the next operator
     operator = 'plus'; 
     input.textContent = ''; 
 });
@@ -141,8 +155,6 @@ times.addEventListener('click', () => {
     input.textContent = ''; 
 });
 
-// adding these functions was key, in this case we can set firstNumber & secondNumber as a & b 
-// which i didnt realize before coding my first attempt at this calculator
 function add(a, b) {
     return a + b;
 }
@@ -177,9 +189,9 @@ function operate(operator, a, b) {
     } 
 }
 
-function arrayMagic() { // this is so we can do operations on more than a pair of number at a time :)
-    numArray.push(firstNumber);
-    if (numArray.length == 2) {
+function arrayMagic() { // this is so we can do operations one pair of numbers at a time
+    numArray.push(firstNumber); // pushes first number
+    if (numArray.length == 2) { // if numArray length is more than 2, operate numbers in numArray & change firstNumber to operated numArray
         let numberOne = Number(numArray[0]);
         let numberTwo = Number(numArray[1]);
         if (operator == 'plus') {
@@ -195,10 +207,32 @@ function arrayMagic() { // this is so we can do operations on more than a pair o
             numArray = [division(numberOne, numberTwo)];
             firstNumber = Number(numArray);
         } else {
-            return;
+            return; // if array length !== 2, move on to next operator
         }
     }
 }
+
+let preventEqualSpam = function() { 
+    if(operator == 'divide' && firstNumber == '') {
+        firstNumber = secondNumber;
+        secondNumber = 1;
+    } else if (operator == 'times' && firstNumber == '') {
+        firstNumber = secondNumber;
+        secondNumber = 1;
+    } else if (operator == 'minus' && firstNumber == '') {
+        firstNumber = secondNumber;
+        secondNumber = ''   
+    } else {
+        return;
+    }
+} 
+// we check if they divided or multiplied right before pressing equal again
+// if they did, firstNumber is = '', so pressing equal would lead to 0 divided/multiplied by secondNumber
+// we fix that by making sure that firstNumber = secondNumber & secondNumber = 1 if user previously multiplied/divided
+// so pressing equal after those operations is gonna = the previous answer
+// if user pressed minus before spamming equal, we wanna switch firstNumber and secondNumber as 0 - secondNumber is = -secondNumber
+// not needed for add as 0 + secondNumber = secondNumber
+
      
 
 
